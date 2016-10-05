39e3971f76261b10016dc26cad08212157052808
Pat Pannuto <ppannuto@umich.edu>
2016-10-05 14:55:48 -0400

    Refactor to eliminate duplicated code

--- rpn.py
+++ rpn.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
 
+import operator
+
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
+}
+
 def calculate(myarg):
 	stack = list()
 	for token in myarg.split():
-		if token == '+':
+		try:
+			token = int(token)
+			stack.append(token)
+		except ValueError:
+			function = operators[token]
 			arg2 = stack.pop()
 			arg1 = stack.pop()
-			result = arg1 + arg2
+			result = function(arg1, arg2)
 			stack.append(result)
-		elif token == '-':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 - arg2
-			stack.append(result)
-		elif token == '*':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 * arg2
-			stack.append(result)
-		elif token == '/':
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = arg1 / arg2
-			stack.append(result)
-		else:
-			stack.append(int(token))
 		print(stack)
 	if len(stack) != 1:
 		raise TypeError("Too many parameters")
